9ff10fb04f37202cdadb55baa895b87e0f5377e0
Peter Zhao <peter.qs.zhao@gmail.com>
2015-12-16 19:25:58 -0500

    PZ add support to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'Installs rbenv and ruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w(centos redhat).each do |os|
+    supports os
+end
