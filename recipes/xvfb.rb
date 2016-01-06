c51ad0cf0f18d8d0951bfaacaadb51ea4017a007
Peter Zhao <peter.qs.zhao@gmail.com>
2016-01-06 11:50:03 -0500

    PZ turn xvfb as a service

--- recipes/xvfb.rb
+++ recipes/xvfb.rb
@@ -1,3 +1,22 @@
 ['xorg-x11-server-Xvfb', 'firefox'].each do |package_name|
   package package_name
 end
+
+cookbook_file '/etc/init/xvfb.conf' do
+  source 'xvfb.conf'
+end
+
+service 'xvfb' do
+  action [:enable, :start]
+  provider Chef::Provider::Service::Upstart 
+end
+
+user_name = node['tools']['user']['name']
+user_home = node['tools']['user']['home']
+
+execute 'export DISPLAY' do
+  command %{echo "export DISPLAY=:99" >> #{user_home}/.bashrc}
+  user user_name
+  group user_name
+  not_if { ::File.read("#{user_home}/.bashrc").include?("export DISPLAY=:99") }
+end
